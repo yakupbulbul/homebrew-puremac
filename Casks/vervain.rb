12ca2696084a06dae6a623d4a86e76b229d14afe
Yakup Bülbül <81179438+yakupbulbul@users.noreply.github.com>
2026-06-01 21:52:24 +0200

    Update SHA-256 for Vervain 1.0.0

--- Casks/vervain.rb
+++ Casks/vervain.rb
@@ -1,6 +1,6 @@
 cask "vervain" do
   version "1.0.0"
-  sha256 "b3d357175bc6555b5680ab8ae84fb28e12c33e923bc071fcb74b415d55991b58"
+  sha256 "c7fd130ffc4089da8db2ae702e3752a8126c248ece191b26d0a6002b244ef9fc"
 
   url "https://github.com/yakupbulbul/Vervain/releases/download/v#{version}/Vervain-#{version}.dmg"
   name "Vervain"
